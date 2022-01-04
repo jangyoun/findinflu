82eb1337f718c07ae4f18885e55c1f42eae30743
jangyoun <jangyounc@gmail.com>
2022-01-05 01:22:10 +0900

    포트폴리오

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import { Button, Typography } from "antd";
 
 function Home() {
   return (
     <div>
-      home
+      <div style={{ width: "100%", height: 200, backgroundColor: "lightgrey", marginBottom: -40 }}></div>
+      <div style={{ width: "100%", display: "flex" }}>
+        <div style={{ width: 80, height: 80, margin: "0px 12px", backgroundColor: "red" }} />
+        <div style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+          <div style={{ marginTop: 12, font: "bold 20px NanumGothic" }}>Jangyoun Lee</div>
+          <div style={{ marginTop: 10, font: "bold 20px NanumGothic" }}>jangyounc@gmail.com</div>
+        </div>
+      </div>
     </div>
   );
 }
